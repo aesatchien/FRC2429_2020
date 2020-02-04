7a36a596138c4062965b898a96108581cb6d0c3f
aesatchien <cjh123@yaoo.com>
2020-02-03 17:37:07 -0800

    adding color sensor functions

--- robot/subsystems/peripherals.py
+++ robot/subsystems/peripherals.py
@@ -8,6 +8,7 @@ from rev.color import ColorMatch
 from wpilib import Color
 from wpilib import I2C
 from wpilib import SmartDashboard
+import math
 
 class Peripherals(Subsystem):
     def __init__(self, robot):
@@ -18,16 +19,17 @@ class Peripherals(Subsystem):
         self.right_dispenser_gate = Servo(8)
         self.counter = 0
         self.color_sensor = ColorSensorV3(I2C.Port.kOnboard)
-        self.color_sensor.setGain(ColorSensorV3.GainFactor.k1x)
+        #self.color_sensor.setGain(ColorSensorV3.GainFactor.k1x)
         self.color_matcher = ColorMatch()
         # we can config the colorsensor resolution and the rate
         #self.color_sensor.configureColorSensor(res=, rate=)
 
         # need to put these numbers in for ourselves by positioning the sensor over the target and recording the RGB
-        self.kBlueTarget = Color(0.182, 0.452, 0.366) #Color(0.143, 0.427, 0.429)
-        self.kGreenTarget = Color(0.218, 0.526, 0.255) #Color(0.197, 0.561, 0.240)
-        self.kRedTarget = Color(0.414, 0.401, 0.185) #Color(0.561, 0.232, 0.114)
-        self.kYellowTarget = Color(0.326, 0.517, 0.155) #Color(0.361, 0.524, 0.113)
+        self.kBlueTarget = Color(0.181, 0.453, 0.365) #Color(0.143, 0.427, 0.429)
+        self.kGreenTarget = Color(0.215, 0.529, 0.255) #Color(0.197, 0.561, 0.240)
+        self.kRedTarget = Color(0.417, 0.398, 0.184) #Color(0.561, 0.232, 0.114)
+        self.kYellowTarget = Color(0.326, 0.519, 0.154) #Color(0.361, 0.524, 0.113)
+        self.color_dict = {"blue":self.kBlueTarget, "green":self.kGreenTarget, "red":self.kRedTarget, "yellow":self.kYellowTarget}
         self.color_matcher.addColorMatch(self.kBlueTarget)
         self.color_matcher.addColorMatch(self.kGreenTarget)
         self.color_matcher.addColorMatch(self.kRedTarget)
@@ -65,8 +67,26 @@ class Peripherals(Subsystem):
                 color_string = 'red'
             elif match == self.kYellowTarget:
                 color_string = 'yellow'
+
+            for key in self.color_dict:
+                match_confidence = self.color_distance(detected_color, self.color_dict[key])
+                if match_confidence < 0.05:
+                    color_string = key
+                    break
+                else:
+                    color_string = "No Match"
+
+
+
             SmartDashboard.putString('Detected Color', color_string)
             SmartDashboard.putNumber("Red", detected_color.red)
             SmartDashboard.putNumber("Green", detected_color.green)
             SmartDashboard.putNumber("Blue", detected_color.blue)
             SmartDashboard.putNumber("Confidence", match_confidence)
+
+    def color_distance(self, color_1, color_2):
+        return math.sqrt(
+            (color_1.red - color_2.red)**2 + \
+               (color_1.green - color_2.green)**2 + \
+               (color_1.blue - color_2.blue)**2
+        )
